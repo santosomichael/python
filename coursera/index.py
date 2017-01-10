2e937bca9d50fbd36238f9e9c4b086b38981064b
michaelsan17 <santosomichael94@gmail.com>
2017-01-10 23:30:44 +0700

    copy folder from coursera to udacity

---
+++ coursera/index.py
@@ -0,0 +1,19 @@
+import pandas as pd
+purchase_1 = pd.Series({'Name': 'Chris',
+                        'Item Purchased': 'Dog Food',
+                        'Cost': 22.50})
+purchase_2 = pd.Series({'Name': 'Kevyn',
+                        'Item Purchased': 'Kitty Litter',
+                        'Cost': 2.50})
+purchase_3 = pd.Series({'Name': 'Vinod',
+                        'Item Purchased': 'Bird Seed',
+                        'Cost': 5.00})
+
+df = pd.DataFrame([purchase_1, purchase_2, purchase_3], index=['Store 1', 'Store 1', 'Store 2'])
+
+print(df)
+# test = df.loc[:,'Cost']*=0.8
+# or
+# test = df.loc['Cost']*=0.8
+# test*=0.8
+# print(test)
